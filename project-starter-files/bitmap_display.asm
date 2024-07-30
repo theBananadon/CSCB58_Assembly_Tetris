6b776ebfa577da66386b9489cffe6c699149c43e
burha <burhanuddindalal22@gmail.com>
2024-07-30 13:22:14 -0400

    fix: text was wrong. Made it right

--- project-starter-files/bitmap_display.asm
+++ project-starter-files/bitmap_display.asm
@@ -6,8 +6,8 @@
 ##############################################################################
 
 ######################## Bitmap Display Configuration ########################
-# - Unit width in pixels: 8
-# - Unit height in pixels: 8
+# - Unit width in pixels: 4
+# - Unit height in pixels: 4
 # - Display width in pixels: 256
 # - Display height in pixels: 256
 # - Base Address for Display: 0x10008000 ($gp)
